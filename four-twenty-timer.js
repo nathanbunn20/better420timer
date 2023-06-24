0a15cf55ca49616d283560b97304f53d0cefe248
nathanbunn20 <nathanbunn20@yahoo.com>
2023-06-24 00:46:12 -0400

    early attempts at optimization

--- four-twenty-timer.js
+++ four-twenty-timer.js
@@ -1,14 +1,15 @@
 const DateTime = luxon.DateTime;
 const timeZones = Intl.supportedValuesOf('timeZone');
 const timerEl = document.getElementById('timer');
+const initialLoadNowUTC = DateTime.now().toUTC();
+
+const morningFourTwentiesOfTheWorldInUTC = timeZones.map((timeZone) => DateTime.fromISO(`${initialLoadNowUTC.setZone(timeZone).toFormat('yyyy-MM-dd')}T04:20:00.000`, { zone: timeZone }).toUTC());
+const afternoonFourTwentiesOfTheWorldInUTC = timeZones.map((timeZone) => DateTime.fromISO(`${initialLoadNowUTC.setZone(timeZone).toFormat('yyyy-MM-dd')}T16:20:00.000`, { zone: timeZone }).toUTC());
+let allFourTwentiesOfTheWorldInUTC = morningFourTwentiesOfTheWorldInUTC.concat(afternoonFourTwentiesOfTheWorldInUTC);
 
 const getClosestFourTwenty = () => {
   const nowInUTC = DateTime.now().toUTC();
 
-  const morningFourTwentiesOfTheWorldInUTC = timeZones.map((timeZone) => DateTime.fromISO(`${nowInUTC.setZone(timeZone).toFormat('yyyy-MM-dd')}T04:20:00.000`, { zone: timeZone }).toUTC());
-  const afternoonFourTwentiesOfTheWorldInUTC = timeZones.map((timeZone) => DateTime.fromISO(`${nowInUTC.setZone(timeZone).toFormat('yyyy-MM-dd')}T16:20:00.000`, { zone: timeZone }).toUTC());
-
-  const allFourTwentiesOfTheWorldInUTC = morningFourTwentiesOfTheWorldInUTC.concat(afternoonFourTwentiesOfTheWorldInUTC);
   const diffBetweenNowAndAllDateTimesInTheWorld = [...new Set(allFourTwentiesOfTheWorldInUTC.map((dt) => dt - nowInUTC))];
   const dateTimesThatAreInTheFuture = diffBetweenNowAndAllDateTimesInTheWorld.filter((diff) => diff > 0);
 
@@ -24,7 +25,26 @@ const getClosestFourTwenty = () => {
 const updateTimer = () => {
   const closestFourTwenty = getClosestFourTwenty();
 
-  timerEl.innerHTML = `hours: ${closestFourTwenty.hours} minutes: ${closestFourTwenty.minutes} seconds: ${Math.round(closestFourTwenty.seconds)} until 4:20`;
+  timerEl.innerHTML = `${closestFourTwenty.hours} hours<br />${closestFourTwenty.minutes} minutes<br />${Math.round(closestFourTwenty.seconds)} seconds<br />until 4:20`;
 };
 
-setInterval(() => { updateTimer(); }, 1000);
+const updateAllFourTwentiesOfTheWorldInUTC = () => {
+  const nowInUTC = DateTime.now().toUTC();
+
+  const fourTwenties = [];
+
+  timeZones.forEach((timeZone) => {
+    const timeZoneDate = nowInUTC.setZone(timeZone).toFormat('yyyy-MM-dd');
+
+    const am = DateTime.fromISO(`${timeZoneDate}T04:20:00.000`, { zone: timeZone }).toUTC();
+    const pm = DateTime.fromISO(`${timeZoneDate}T16:20:00.000`, { zone: timeZone }).toUTC();
+
+    fourTwenties.push(am);
+    fourTwenties.push(pm);
+  });
+  
+  allFourTwentiesOfTheWorldInUTC = fourTwenties;
+};
+
+setInterval(() => { updateTimer(); }, 1000); // every second
+setInterval(() => { updateAllFourTwentiesOfTheWorldInUTC(); }, 60000); // every minute, I don't think this works since we aren't using a cron schedule. gotta do it every second probably :(
