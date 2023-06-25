23afd1b13c06493698a178c76c5abfdbdaf802b5
Nate <nathanbunn20@yahoo.com>
2023-06-24 23:33:22 -0400

    Optimized resources and CPU usage (#4)
    * early attempts at optimization
    
    * added codeowners file
    
    * got things running a little faster by reducing the amount of math being done... by getting unique 4:20 times
    
    * adding thoughts on how to best optimize this process
    
    * optimized pretty well now
    
    * added readme documentation
    
    * typo

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # better420timer
-Timer that lets you know if html (using id="timer") every second how soon it is until 4:20 somewhere in the world
+A timer built in html css & pure javascript that lets you know how soon it is until 4:20 somewhere in the world.
 
 Only dependency is the minified [luxon package](https://moment.github.io/luxon/api-docs/index.html)
+
+The better420timer is optimized to collect today and tomorrow's 4:20 times all around the world on initial load of the page. Then every 24 hours the better420timer will refresh that list of 4:20 times (for that day and the next). With that information, the better420timer compares "now" with the list of 4:20 times and selects the closest 4:20 time that hasn't occured yet.
+
+### Usage
+The better420timer replaces the entire innerHTML of the div elements with the id's "hours", "minutes", and "seconds" **with the numeric value for that time component**, so anything you put in those div's will be overwritten by the program. You'll have to work around those div's to style it how you want to.
+
+*Heads up: The hours div as well as the div that the hours is inside (the parent div) will be hidden if the hours is 0 **AND** the parent div is not the timer (the div with the class="timer"). This is so that the label for hours get's hidden too. If you do not want this behavior, then you'll have to edit the javascript (four-twenty-timer.js):*
+
+Find this
+```js
+if (hoursEl.parentElement.classList.contains('timer')) {
+    hoursEl.style.display = 'none';
+} else {
+    hoursEl.parentElement.style.display = 'none';
+}
+```
+
+Delete all of that and replace with this
+```js
+hoursEl.style.display = 'none';
+```
--- four-twenty-timer.js
+++ four-twenty-timer.js
@@ -1,15 +1,19 @@
 const DateTime = luxon.DateTime;
 const timeZones = Intl.supportedValuesOf('timeZone');
 const timerEl = document.getElementById('timer');
+const hoursEl = document.getElementById('hours');
+const initialHoursDisplay = hoursEl.style.display;
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+const oneSecondInMilliseconds = 1000;
+const twentyFourHoursInMilliseconds = 86400000;
+
+let uniqueFourTwentiesInTheWorld = [];
 
 const getClosestFourTwenty = () => {
   const nowInUTC = DateTime.now().toUTC();
 
-  const morningFourTwentiesOfTheWorldInUTC = timeZones.map((timeZone) => DateTime.fromISO(`${nowInUTC.setZone(timeZone).toFormat('yyyy-MM-dd')}T04:20:00.000`, { zone: timeZone }).toUTC());
-  const afternoonFourTwentiesOfTheWorldInUTC = timeZones.map((timeZone) => DateTime.fromISO(`${nowInUTC.setZone(timeZone).toFormat('yyyy-MM-dd')}T16:20:00.000`, { zone: timeZone }).toUTC());
-
-  const allFourTwentiesOfTheWorldInUTC = morningFourTwentiesOfTheWorldInUTC.concat(afternoonFourTwentiesOfTheWorldInUTC);
-  const diffBetweenNowAndAllDateTimesInTheWorld = [...new Set(allFourTwentiesOfTheWorldInUTC.map((dt) => dt - nowInUTC))];
+  const diffBetweenNowAndAllDateTimesInTheWorld = uniqueFourTwentiesInTheWorld.map((dt) => dt - nowInUTC);
   const dateTimesThatAreInTheFuture = diffBetweenNowAndAllDateTimesInTheWorld.filter((diff) => diff > 0);
 
   const sortedDateTimes = dateTimesThatAreInTheFuture.sort((a, b) => {
@@ -24,7 +28,62 @@ const getClosestFourTwenty = () => {
 const updateTimer = () => {
   const closestFourTwenty = getClosestFourTwenty();
 
-  timerEl.innerHTML = `hours: ${closestFourTwenty.hours} minutes: ${closestFourTwenty.minutes} seconds: ${Math.round(closestFourTwenty.seconds)} until 4:20`;
+  let hours = closestFourTwenty.hours;
+  let minutes = closestFourTwenty.minutes;
+  let seconds = Math.round(closestFourTwenty.seconds);
+
+  if (seconds === 60) {
+    minutes++;
+    seconds = 0;
+  }
+
+  if (hours === 0) {
+    if (hoursEl.parentElement.classList.contains('timer')) {
+      hoursEl.style.display = 'none';
+    } else {
+      hoursEl.parentElement.style.display = 'none';
+    }
+  } else {
+    hours.style.display = initialHoursDisplay;
+    hours.innerHTML = hours;
+  }
+
+  minutesEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
 };
 
-setInterval(() => { updateTimer(); }, 1000);
+const updateAllFourTwentiesOfTheWorldInUTC = () => {
+  const nowInUTC = DateTime.now().toUTC();
+  const fourTwenties = [];
+
+  timeZones.forEach((timeZone) => {
+    const todaysTimeZoneDate = nowInUTC.setZone(timeZone).toFormat('yyyy-MM-dd');
+    const tomorrowsTimeZoneDate = nowInUTC.plus({ days: 1 }).setZone(timeZone).toFormat('yyyy-MM-dd');
+
+    const todaysMorningFourTwenties = DateTime.fromISO(`${todaysTimeZoneDate}T04:20:00.000`, { zone: timeZone }).toUTC();
+    const todaysAfternoonFourTwenties = DateTime.fromISO(`${todaysTimeZoneDate}T16:20:00.000`, { zone: timeZone }).toUTC();
+
+    const tomorrowsMorningFourTwenties = DateTime.fromISO(`${tomorrowsTimeZoneDate}T04:20:00.000`, { zone: timeZone }).toUTC();
+    const tomorrowsAfternoonFourTwenties = DateTime.fromISO(`${tomorrowsTimeZoneDate}T16:20:00.000`, { zone: timeZone }).toUTC();
+
+    fourTwenties.push(todaysMorningFourTwenties);
+    fourTwenties.push(todaysAfternoonFourTwenties);
+    fourTwenties.push(tomorrowsMorningFourTwenties);
+    fourTwenties.push(tomorrowsAfternoonFourTwenties);
+  });
+  
+  uniqueFourTwentiesInTheWorld = [...new Map(fourTwenties.map(dt => [dt['ts'], dt])).values()];;
+};
+
+const init = () => {
+  updateAllFourTwentiesOfTheWorldInUTC();
+
+  updateTimer();
+
+  timerEl.classList.remove('hidden-on-load');
+
+  setInterval(() => { updateTimer(); }, oneSecondInMilliseconds);
+  setInterval(() => { updateAllFourTwentiesOfTheWorldInUTC(); }, twentyFourHoursInMilliseconds);
+};
+
+init();
